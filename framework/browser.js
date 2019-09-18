7ef9f7d9ac45d006ccdcbfd62356f113ef204c3b
VladKostyukevich <vlad.kostyukevich@gmail.com>
2019-09-18 10:40:54 +0300

    no message

--- framework/browser.js
+++ framework/browser.js
@@ -2,7 +2,7 @@ const webdriver = require('selenium-webdriver');
 const publicURL = 'localhost'
 const getDriver = () => new webdriver.Builder()
 .forBrowser('firefox')
-.usingServer(`https://${publicURL}:4444/wd/hub`)
+.usingServer(`http://${publicURL}:4444/wd/hub`)
 .build();
 
 module.exports = getDriver;
